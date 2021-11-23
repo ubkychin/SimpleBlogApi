1d70c0ace831c7ba62ff3a94d76a39aede908f9f
Anh Nguyen <anguyen@swin.edu.au>
2021-11-23 11:28:13 +1100

    Ready for Deployment

--- SimpleBlogApiTests/BlogTests.cs
+++ SimpleBlogApiTests/BlogTests.cs
@@ -15,10 +15,11 @@ namespace SimpleBlogApiTests
 
         [Theory]
         [InlineData("X4X8aJI1nD0S2z7lpPwaNjZB0nGPjrRW0IUabNNeeZ15tFMG4Y2axA9EsSjil4vpHBT1ju", "X4X8aJI1nD0S2z7lpPwaNjZB0nGPjrRW0IUabNNeeZ15tFMG4Y2axA9EsSjil4vpHBT1ju")]
-        [InlineData("tksghm63r3QZjsDSCqaiEIAdW3tl2wzeT0fkyen5Wk2xCDhDzWMYU6ZO5bnHkcyRVbcodHS", "tksghm63r3QZjsDSCqaiEIAdW3tl2wzeT0fkyen5Wk2xCDhDzWMYU6ZO5bnHkcyRVbcodH")]        
+        [InlineData("tksghm63r3QZjsDSCqaiEIAdW3tl2wzeT0fkyen5Wk2xCDhDzWMYU6ZO5bnHkcyRVbcodH", "tksghm63r3QZjsDSCqaiEIAdW3tl2wzeT0fkyen5Wk2xCDhDzWMYU6ZO5bnHkcyRVbcodHS")]        
         public void CheckLengthTest(string expected, string input)
         {
             testPost = new Post("123", input, "content", DateTime.Now);
+            testPost.CheckTitleLength();
             Assert.Equal(expected, testPost.Title);
             Assert.Equal(expected.Length, testPost.Title.Length);
         }
